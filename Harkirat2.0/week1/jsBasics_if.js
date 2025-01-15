abe3fe6e0736ed781dfd1aae33425e83e8cffdf6
Neeraj Kumar <72290172+DrunkTrader@users.noreply.github.com>
2025-01-15 06:59:54 +0000

    if statement

---
+++ Harkirat2.0/week1/jsBasics_if.js
@@ -0,0 +1,10 @@
+//A program that tells a person is married or not with there firstName attached to it.
+let firstName = "Neeraj";
+let age = 18;
+let isMarried = false;
+if (isMarried == true) {
+  console.log(firstName + " is a Married person.")
+}
+if (isMarried == false) {
+  console.log(firstName + " is not a Married person.")
+}
